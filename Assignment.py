bcc6481e74b2ad3b1c0b6e0ee56a037bef417852
Tanko221 <52928308+Tanko221@users.noreply.github.com>
2019-07-17 17:03:06 +0000

    Add files via upload

--- Assignment.py
+++ Assignment.py
@@ -1,16 +1,4 @@
-print("Labwork")
-print('Labwork is a good initiative')
-print("labwork is a good initiative \n I like the idea thus far")
-print("Labwork is a good initiative I like the idea thus far")
-a="labwork is good"
-print(len(a))
-b="I like the idea"
-print(a+b)
-c= "Ibrahim, Babangida"
-print(a.split(","))
-ss="Bazinga"
-print(ss[2:6])
-
-
-
-
+act1=["Ibrahim","Babangida","Current",1000]
+act2=["Salim","Tanko","Savings",1500]
+act3=["Radiya","Hukuma","Current",2000]
+b_accts={"001":act1,"002":act2,"003":act3}
