91f7d685c08d5f435c9c63cbb1005747cdfb7a71
Tanko221 <tankorauf@gmail.com>
2019-07-17 12:35:35 +0000

    Day2

---
+++ Assignment.py
@@ -0,0 +1,16 @@
+print("Labwork")
+print('Labwork is a good initiative')
+print("labwork is a good initiative \n I like the idea thus far")
+print("Labwork is a good initiative I like the idea thus far")
+a="labwork is good"
+print(len(a))
+b="I like the idea"
+print(a+b)
+c= "Ibrahim, Babangida"
+print(a.split(","))
+ss="Bazinga"
+print(ss[2:6])
+
+
+
+
